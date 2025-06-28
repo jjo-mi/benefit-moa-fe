c0ff28eee52b0df879d393199174f87dff0d866e
jjo-mi <whdal2636@naver.com>
2025-06-28 23:56:28 +0900

    [style] input 컴포넌트

---
+++ src/conponent/ui/Input.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface InputProps {
+  type: string;
+  placeholder: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export const Input = ({ type, placeholder, value, onChange }: InputProps) => {
+  return <input type={type} placeholder={placeholder} value={value} onChange={onChange} />;
+};
